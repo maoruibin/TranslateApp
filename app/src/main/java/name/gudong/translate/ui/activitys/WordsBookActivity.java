cf3befe22a514246ae8d1205eaa8113c0522c15c
Gudong <gudong.name@gmail.com>
2017-03-04 19:22:03 +0800

    update gradle version to 2.3 and fix share words bug

--- app/src/main/java/name/gudong/translate/ui/activitys/WordsBookActivity.java
+++ app/src/main/java/name/gudong/translate/ui/activitys/WordsBookActivity.java
@@ -140,7 +140,7 @@ public class WordsBookActivity extends BaseActivity<BookPresenter> implements IB
                         .setPositiveButton("发送", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Utils.shareText(getBaseContext(), exportText);
+                                Utils.shareText(WordsBookActivity.this, exportText);
                             }
                         })
                         .setNegativeButton("取消", null)
